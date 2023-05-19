7bb4acac48e3c19d59abd0a8c7347e6850e4c754
BaoBui <bao.bui@fastboy.net>
2023-05-19 17:49:33 +0700

    --

--- smart_home_dashboard_tablet/lib/main.dart
+++ smart_home_dashboard_tablet/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(const App());
 }
 // https://cdn.dribbble.com/users/6259454/screenshots/16785507/media/82aa1573e2451d300d0b1e415b57af91.png
+// https://dribbble.com/shots/15784670-Smart-Home-App/attachments/7593078?mode=media
 class App extends StatelessWidget {
   final m3BaseColors = const Color(0xFF40C0FE);
   const App({super.key});
